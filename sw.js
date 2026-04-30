0021813448fa5612db539295a1d3b2a5f13cf815
Andriy Gavrylenko <gavrosh2007@gmail.com>
2026-04-30 10:41:26 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/offline.html',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192x192.png',
+  '/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
