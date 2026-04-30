e979815f5a18fc6dbc40648821fbcf09e9c70cb6
Andriy Gavrylenko <gavrosh2007@gmail.com>
2026-04-30 19:12:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,45 @@
-const CACHE_NAME = 'hs-market-v2';
+const CACHE_NAME = 'hs-market-v3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/offline.html',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  '.',
+  'index.html',
+  'manifest.json',
+  'offline.html',
+  'icon-192x192.png',
+  'icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request).then(response => {
-        if (response) return response;
-        if (event.request.mode === 'navigate') {
-          return caches.match('/offline.html');
-        }
-        return null;
-      });
-    })
-  );
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(event.request).then(response => {
+          return response || caches.match('offline.html');
+        });
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
